f91ee38d4a47552a2e55fe038cacbf77506449d2
ltvnkvtl <vital.litovk@gmail.com>
2020-06-23 08:03:28 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {environment} from "../environments/environment";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6L-_6ZsH0djIN3WLXtwFrOAim_2UhnY4",
+    authDomain: "angular-shop-78e4d.firebaseapp.com",
+    databaseURL: "https://angular-shop-78e4d.firebaseio.com",
+    projectId: "angular-shop-78e4d",
+    storageBucket: "angular-shop-78e4d.appspot.com",
+    messagingSenderId: "501626649992",
+    appId: "1:501626649992:web:0652e61bfcc1c2fd2a8e3d"
+  }
 };
