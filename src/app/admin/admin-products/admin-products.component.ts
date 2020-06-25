81ad13babb62085ea6bbccb16f39e16a3b02691b
ltvnkvtl <vital.litovk@gmail.com>
2020-06-25 09:58:20 +0300

    added filtration

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../../product.service";
+import {Subscription} from "rxjs";
+import {Product} from "../../models/product";
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database'
+import {Product} from "./models/product";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,12 @@ export class ProductService {
   }
 
   getAll() {
-    return this.db.list('/products').snapshotChanges();
+    return this.db.list<Product>('/products').snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(a => ({ key: a.key, ...a.payload.val() }))
+        )
+      );
   }
 
   get(productId) {
